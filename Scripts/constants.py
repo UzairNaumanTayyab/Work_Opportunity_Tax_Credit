0bcf76816041596d1826e05ad824ef193285b4d5
Your Name <uzairnauman17@gmail.com>
2025-12-17 03:10:27 -0500

    First Commit with only one dataset

---
+++ Scripts/constants.py
@@ -0,0 +1,27 @@
+# Scripts/constants.py
+
+POSITIONS = ["LPN", "RN", "CNA", "CHHA", "ADMIN", "CG"]
+
+RATINGS = [
+    "0 - Not Applicable",
+    "1 - Unacceptable",
+    "3 - Meets Expectations",
+    "4 - Exceeds Expectations"
+]
+
+EDUCATION = [
+    "High School",
+    "Associate Degree",
+    "4 year College"
+]
+
+LANGUAGES = [
+    "English",
+    "English, Spanish",
+    "English, French"
+]
+
+TERMINATION_STATUSES = [
+    "Resigned/Separated - Eligible for Rehire",
+    "Resigned/Separated - Not Eligible for Rehire"
+]
